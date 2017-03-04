a8fa678b0f396fa9d3604d58154e4bb569bee933
itggot-Harry-Norrman <harry.norrman@itggot.se>
2017-03-04 15:10:36 +0100

    Everything

---
+++ site/js/main.js
@@ -0,0 +1,17 @@
+  function menu() {
+    document.querySelector(".menu").classList.add("anim");
+    document.querySelector(".remove").classList.add("shade")
+  }
+  function remen() {
+    document.querySelector(".menu").classList.remove("anim")
+    document.querySelector(".remove").classList.remove("shade")
+  }
+  function search() {
+    document.querySelector(".search").classList.add("srcopc")
+  }
+  function remsearch() {
+    document.querySelector(".search").classList.remove("srcopc")
+  }
+  function dismiss() {
+    document.querySelector(".mainart").classList.add('remart');
+  }
